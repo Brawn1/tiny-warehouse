4c32bdfc754c11ac11a1f1422bdb0dbb462ce127
Guenter Bailey <guenter.bailey@bailey-solution.com>
2024-02-12 16:03:43 +0100

    move into own app

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "warehouse.apps.WarehouseConfig"
+    "warehouse.apps.WarehouseConfig",
+    "electronic.apps.ElectronicConfig"
 ]
 
 MIDDLEWARE = [
--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
-from .models import Manufacturer, Warehouse, AssortmentBox, PartCategory, Part, Compartment
+from .models import Manufacturer, Warehouse
 
 admin.site.register(Manufacturer)
 admin.site.register(Warehouse)
-admin.site.register(AssortmentBox)
-admin.site.register(PartCategory)
-admin.site.register(Part)
-
-
-class CompartmentAdmin(admin.ModelAdmin):
-    list_display = ('number', 'part', 'quantity', 'assortment_box')
-    search_fields = ('number', 'part__name', 'assortment_box__name')
-
-
-admin.site.register(Compartment, CompartmentAdmin)
