40b849ffa0201100d70ccf5051783d59e41175f0
Guenter Bailey <guenter.bailey@bailey-solution.com>
2024-02-12 23:36:39 +0100

    added form and last fields as number

--- electronic/admin.py
+++ electronic/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from .models import AssortmentBox, PartCategory, Part, Compartment
 
@@ -7,10 +8,23 @@ admin.site.register(PartCategory)
 admin.site.register(Part)
 
 
+class CompartmentForm(forms.ModelForm):
+    class Meta:
+        model = Compartment
+        fields = ['number', 'part', 'quantity', 'assortment_box']
+
+    def __init__(self, *args, **kwargs):
+        super(CompartmentForm, self).__init__(*args, **kwargs)
+        query = Compartment.objects.all().latest('id')
+        self.fields['number'].initial = query.number + 1
+        self.fields['assortment_box'].initial = query.assortment_box
+
+
 class CompartmentAdmin(admin.ModelAdmin):
     list_display = ('number', 'part', 'quantity', 'assortment_box')
     search_fields = ('number', 'part__name', 'assortment_box__name')
     list_filter = ('assortment_box', 'part__manufacturer', 'part__category')
+    form = CompartmentForm
 
 
 admin.site.register(Compartment, CompartmentAdmin)
